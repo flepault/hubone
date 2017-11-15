cf9d6189ed4db167445cd5ab827937f046067ba2
flepault <flepault@gmail.com>
2017-11-15 15:29:40 +0100

    Commit Final packaging dev

---
+++ TransformationTools/docs/MigrationToolsReadMe.txt
@@ -0,0 +1,5 @@
+1- Copy MigrationTools.zip into D:
+2- Unzip it in D:
+3- Deploy input data from Hub One into D:\MigrationTools\TransformationTools\input
+4- Run RunMigration.bat script
+5- During Transformation step follow progress here : http://localhost:8161/admin/queues.jsp (admin/admin)
--- TransformationTools/scripts/RunMigration.bat
+++ TransformationTools/scripts/RunMigration.bat
@@ -1,19 +1,70 @@
 @echo off
 
+echo "###############################################"
+echo "############### START ACTIVE MQ ###############"
+echo "###############################################"
+cd D:\\MigrationTools\\TransformationTools\\apache-activemq-5.15.0\\bin
+start activemq-admin.bat start
+echo "###############################################"
+echo "############## ACTIVE MQ STARTED ##############"
+echo "###############################################"
+
+echo "###############################################"
+echo "########## RUN TRANSFORMATION TOOLS ###########"
+echo "###############################################"
 cd D:\\MigrationTools\\TransformationTools\\
 cmd /c RunTransformationTools.bat
+echo "###############################################"
+echo "######### TRANSFORMATION TOOLS ENDED ##########"
+echo "###############################################"
+pause
 
+echo "###############################################"
+echo "########### DEPLOY TRANSFORMED DATA ###########"
+echo "###############################################"
 cd D:\\MigrationTools\\TransformationTools\\output\\
 cmd /c D:\\MigrationTools\\TransformationTools\\output\\DeployOutputFile.bat
+echo "###############################################"
+echo "########## TRANSFORMED DATA DEPLOYED ##########"
+echo "###############################################"
+pause
 
+echo "###############################################"
+echo "##### DEPLOY ECB MIGRATION TOOLS BINARIES #####"
+echo "###############################################"
 cd D:\\MigrationTools\\ECBDataMigration\\Bin\\
 cmd /c DeployBin.bat
+echo "###############################################"
+echo "#### ECB MIGRATION TOOLS BINARIES DEPLOYED ####"
+echo "###############################################"
+pause
 
+echo "###############################################"
+echo "############### RUN ECB MAPPER  ###############"
+echo "###############################################"
 cd D:\\MigrationTools\\ECBDataMigration\\Mapper\\
 cmd /c RunMapper.bat
+echo "###############################################"
+echo "############## ECB MAPPER ENDED  ##############"
+echo "###############################################"
+pause
 
+echo "###############################################"
+echo "############# DEPLOY MAPPED DATA  #############"
+echo "###############################################"
 cd D:\\MigrationTools\\ECBDataMigration\\Mapper\\output\\
 cmd /c DeployOutputFile.bat
+echo "###############################################"
+echo "############# MAPPED DATA DEPLOYED ############"
+echo "###############################################"
+pause
 
+echo "###############################################"
+echo "############### RUN ECB LOADER  ###############"
+echo "###############################################"
 cd D:\\MigrationTools\\ECBDataMigration\\Loader\\
 cmd /c RunLoader.bat
+echo "###############################################"
+echo "############### ECB LOADER ENDED ##############"
+echo "###############################################"
+pause
