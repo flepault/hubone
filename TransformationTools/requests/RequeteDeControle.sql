45748befb10a19f09499570d68eb1bc79ad78a81
efrelep <efrelep@FR00115428.ericsson.se>
2018-03-23 16:26:03 +0100

    Commit 23/03/2018

--- TransformationTools/requests/RequeteDeControle.sql
+++ TransformationTools/requests/RequeteDeControle.sql
@@ -69,8 +69,7 @@ or pv.nm_name = 'hubone.fr/OrangeAbo' or pv.nm_name = 'hubone.fr/RampBucket'
 or pv.nm_name = 'hubone.fr/Tetra' or pv.nm_name = 'hubone.fr/Voix' 
 or pv.nm_name = 'hubone.fr/WifiRoaming')
 and inter.tx_interval_status = 'O' and acu.id_usage_interval = inter.id_interval
-and GETDATE()> dt_end
- 
+and GETDATE()> dt_end 
  
 --Chiffre d’affaire global HT et chiffre d’affaire global TTC.
 select sum(invoice_amount-tax_ttl_amt) as HT,sum(tax_ttl_amt) as TVA,sum(invoice_amount) as TTC 
@@ -83,13 +82,36 @@ invoice_string as 'NO_FACTURE',
 invoice_amount as 'MONTANT_TOTAL' ,
 tax_ttl_amt as 'MONTANT_TVA',
 invoice_amount-tax_ttl_amt as 'MONTANT_TOTAL_HT',
-(select sum(amount) from #RecTmpTable where id_acc = inv.id_acc ) as 'MONTANT_ABO',
-(select sum(amount) from #NRecTmpTable where id_acc = inv.id_acc ) as 'MONTANT_FAS',
-(select sum(amount) from #DiscountTmpTable where id_acc = inv.id_acc ) as 'MONTANT_REM',
-(select sum(amount) from #UsageTmpTable where id_acc = inv.id_acc ) as 'MONTANT_USG'
+(select sum(amount) from #RecTmpTable rec where rec.id_acc = inv.id_acc 
+and rec.id_sess not in ( select id_sess from t_cust_usage_correction cor, t_invoice i  
+						 where cor.id_invoice_num = i.id_invoice_num and i.id_acc = inv.id_acc and i.id_invoice_num!=inv.id_invoice_num)) as 'MONTANT_ABO',
+(select sum(amount) from #NRecTmpTable nrec where nrec.id_acc = inv.id_acc 
+and nrec.id_sess not in ( select id_sess from t_cust_usage_correction cor, t_invoice i  
+						  where cor.id_invoice_num = i.id_invoice_num and i.id_acc = inv.id_acc and i.id_invoice_num!=inv.id_invoice_num)) as 'MONTANT_FAS',
+(select sum(amount) from #DiscountTmpTable disc where disc.id_acc = inv.id_acc 
+and disc.id_sess not in ( select id_sess from t_cust_usage_correction cor, t_invoice i  
+						  where cor.id_invoice_num = i.id_invoice_num and i.id_acc = inv.id_acc and i.id_invoice_num!=inv.id_invoice_num)) as 'MONTANT_REM',
+(select sum(amount) from #UsageTmpTable usg where usg.id_acc = inv.id_acc 
+and usg.id_sess not in ( select id_sess from t_cust_usage_correction cor, t_invoice i 
+						 where cor.id_invoice_num = i.id_invoice_num and i.id_acc = inv.id_acc and i.id_invoice_num!=inv.id_invoice_num)) as 'MONTANT_USG'
+from t_invoice inv, t_account_mapper map, t_account acc1, t_av_Common avC
+where inv.id_acc = map.id_acc and map.id_acc = acc1.id_acc and acc1.id_acc = avC.id_acc
+and invoice_string like 'L%'
+union
+select 'C'+avC.c_ClientRootId as 'CODE_CLIENT_GENERIQUE',
+IIF(acc1.id_type=12, 'C', 'F')+map.nm_login as 'CODE_CLIENT_FACTURE', 
+invoice_string as 'NO_FACTURE',
+invoice_amount as 'MONTANT_TOTAL' ,
+tax_ttl_amt as 'MONTANT_TVA',
+invoice_amount-tax_ttl_amt as 'MONTANT_TOTAL_HT',
+(select sum(amount) from #RecTmpTable rec where rec.id_acc = inv.id_acc 
+and rec.id_sess in ( select id_sess from t_cust_usage_correction cor  where cor.id_invoice_num = inv.id_invoice_num)) as 'MONTANT_ABO',
+0 as 'MONTANT_FAS',
+0 as 'MONTANT_REM',
+0 as 'MONTANT_USG'
 from t_invoice inv, t_account_mapper map, t_account acc1, t_av_Common avC 
 where inv.id_acc = map.id_acc and map.id_acc = acc1.id_acc and acc1.id_acc = avC.id_acc
-
+and invoice_string like 'R%'
 
 --Chiffre d’affaire factures régulières et de régularisation
 select  'Facture régulières' ,COALESCE(sum(invoice_amount),0) as 'Montant TTC' from t_invoice where invoice_string like 'L%'
